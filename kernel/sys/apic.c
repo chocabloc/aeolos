3cf986759e1d6b97a74525bde068f96211c7317d
aditya <aarshisback@gmail.com>
2020-12-11 10:58:41 +0530

    added apic_write_reg

--- kernel/sys/apic.c
+++ kernel/sys/apic.c
@@ -11,12 +11,16 @@ uint32_t apic_read_reg(uint16_t offset)
     return *(uint32_t*)(lapic_base + offset);
 }
 
+void apic_write_reg(uint16_t offset, uint32_t val)
+{
+    *(uint32_t*)(lapic_base + offset) = val;
+}
+
 void apic_init()
 {
     kdbg_warn("apic_init(): STUB\n");
 
     madt_init();
     lapic_base = (void*)PHYS_TO_VIRT(madt_get_lapic_base());
-
-    kdbg_info("APIC Version: %x\n", apic_read_reg(APIC_REG_VERSION));
+    vmm_map((uint64_t)lapic_base, VIRT_TO_PHYS(lapic_base), 1);
 }
--- kernel/sys/apic.h
+++ kernel/sys/apic.h
@@ -4,5 +4,8 @@
 
 #define APIC_REG_ID 0x20
 #define APIC_REG_VERSION 0x30
+#define APIC_REG_SPURIOUS_INT 0xF0
+#define APIC_REG_LVT_TIMER 0x320
+#define APIC_REG_EOI 0xB0
 
-void apic_init();
+void apic_init(void);
