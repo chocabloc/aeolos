1a868c8709154f75729357555370b4681657572e
aditya <aarshisback@gmail.com>
2020-12-11 12:08:09 +0530

    improved stack traces

--- kernel/sys/panic.c
+++ kernel/sys/panic.c
@@ -1,18 +1,18 @@
 #include "panic.h"
 #include "dev/term/term.h"
 #include "kconio.h"
-#include "mm/pmm.h"
 #include "symbols.h"
 #include <stdarg.h>
 #include <stdbool.h>
 
+extern void kernel_start;
 extern void kernel_end;
 
 // get function name from address, using the symbol table
 static const char* symtab_get_func(uint64_t addr)
 {
     for (int i = 0; _kernel_symtab[i].addr < UINT64_MAX; i++)
-        if (_kernel_symtab[i].addr < addr && _kernel_symtab[i + 1].addr > addr)
+        if (_kernel_symtab[i].addr < addr && _kernel_symtab[i + 1].addr >= addr)
             return _kernel_symtab[i].name;
 
     return "Unknown Function";
@@ -28,8 +28,8 @@ static void do_stacktrace()
     kprintf("\nStack Trace:\n");
     for (int i = 0;; i++) {
         kprintf(" \t%d: ", i);
-        if ((uint64_t)rbp_val <= HIGHERHALF_OFFSET || (void*)rbp_val >= &kernel_end) {
-            kprintf("<Invalid Base Pointer>");
+        if ((void*)rbp_val < &kernel_start || (void*)rbp_val >= &kernel_end) {
+            kprintf("\t<Invalid Base Pointer>");
             break;
         }
         uint64_t func_addr = *(rbp_val + 1);
