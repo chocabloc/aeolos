74deab152054f3b6f4972ea8b05e5449be3d522b
aditya <aarshisback@gmail.com>
2020-12-21 10:42:24 +0530

    added cpuid

---
+++ kernel/sys/cpuid.c
@@ -0,0 +1,33 @@
+#include "cpuid.h"
+
+void cpuid(uint32_t func, uint32_t param, uint32_t* eax, uint32_t* ebx, uint32_t* ecx, uint32_t* edx)
+{
+    asm volatile("mov %[func], %%eax;"
+                 "mov %[param], %%ecx;"
+                 "cpuid;"
+                 "mov %%eax, %[ieax];"
+                 "mov %%ebx, %[iebx];"
+                 "mov %%ecx, %[iecx];"
+                 "mov %%edx, %[iedx];"
+                 : [ ieax ] "=g"(*eax), [ iebx ] "=g"(*ebx), [ iecx ] "=g"(*ecx), [ iedx ] "=g"(*edx)
+                 : [ func ] "g"(func), [ param ] "g"(param)
+                 : "%eax", "%ebx", "%ecx", "%edx", "memory");
+}
+
+bool cpuid_check_feature(cpuid_feature_t feature)
+{
+    uint32_t regs[4];
+    cpuid(feature.func, 0, &regs[cpuid_eax], &regs[cpuid_ebx], &regs[cpuid_ecx], &regs[cpuid_edx]);
+
+    if (regs[feature.reg] & feature.mask)
+        return true;
+
+    return false;
+}
+
+void cpuid_get_vendor(char* vendor)
+{
+    uint32_t _eax;
+    cpuid(0, 0, &_eax, (uint32_t*)vendor, (uint32_t*)(vendor + 8), (uint32_t*)(vendor + 4));
+    vendor[12] = '\0';
+}
---
+++ kernel/sys/cpuid.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+    uint32_t func;
+    enum {
+        cpuid_eax,
+        cpuid_ebx,
+        cpuid_ecx,
+        cpuid_edx
+    } reg;
+    uint32_t mask;
+} cpuid_feature_t;
+
+static const cpuid_feature_t CPUID_FEATURE_SSE1 = { .func = 0x00000001, .reg = cpuid_edx, .mask = 1 << 25 };
+static const cpuid_feature_t CPUID_FEATURE_SSE2 = { .func = 0x00000001, .reg = cpuid_edx, .mask = 1 << 26 };
+static const cpuid_feature_t CPUID_FEATURE_SSE3 = { .func = 0x00000001, .reg = cpuid_ecx, .mask = 1 << 0 };
+static const cpuid_feature_t CPUID_FEATURE_SSSE3 = { .func = 0x00000001, .reg = cpuid_ecx, .mask = 1 << 9 };
+static const cpuid_feature_t CPUID_FEATURE_SSE41 = { .func = 0x00000001, .reg = cpuid_ecx, .mask = 1 << 19 };
+static const cpuid_feature_t CPUID_FEATURE_SSE42 = { .func = 0x00000001, .reg = cpuid_ecx, .mask = 1 << 20 };
+static const cpuid_feature_t CPUID_FEATURE_POPCNT = { .func = 0x00000001, .reg = cpuid_ecx, .mask = 1 << 23 };
+static const cpuid_feature_t CPUID_FEATURE_LZCNT = { .func = 0x80000001, .reg = cpuid_ecx, .mask = 1 << 5 };
+static const cpuid_feature_t CPUID_FEATURE_AVX = { .func = 0x00000001, .reg = cpuid_ecx, .mask = 1 << 28 };
+static const cpuid_feature_t CPUID_FEATURE_AVX2 = { .func = 0x00000007, .reg = cpuid_ebx, .mask = 1 << 5 };
+
+void cpuid(uint32_t func, uint32_t param, uint32_t* eax, uint32_t* ebx, uint32_t* ecx, uint32_t* edx);
+bool cpuid_check_feature(cpuid_feature_t feature);
+void cpuid_get_vendor(char* vendor);
