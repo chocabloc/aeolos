b3176c5291e0154ffd468f77b700462ba12f6548
aditya <aarshisback@gmail.com>
2020-12-22 11:15:59 +0530

    added double buffering support

--- kernel/dev/fb/fb.c
+++ kernel/dev/fb/fb.c
@@ -1,7 +1,13 @@
 #include "fb.h"
+#include "kmalloc.h"
+#include "memutils.h"
 #include "mm/vmm.h"
+#include <stdbool.h>
 
 static fb_info fb;
+
+static bool isdoublebuffered;
+static uint8_t* backbuffer;
 
 const fb_info* fb_getinfo()
 {
@@ -10,12 +16,12 @@ const fb_info* fb_getinfo()
 
 void fb_putpixel(uint32_t x, uint32_t y, uint32_t color)
 {
-    ((uint32_t*)(fb.addr + (fb.pitch * y)))[x] = color;
+    ((uint32_t*)(backbuffer + (fb.pitch * y)))[x] = color;
 }
 
 uint32_t fb_getpixel(uint32_t x, uint32_t y)
 {
-    return ((uint32_t*)(fb.addr + (fb.pitch * y)))[x];
+    return ((uint32_t*)(backbuffer + (fb.pitch * y)))[x];
 }
 
 void fb_init(stv2_struct_tag_fb* t)
@@ -24,4 +30,21 @@ void fb_init(stv2_struct_tag_fb* t)
     fb.width = t->fb_width;
     fb.height = t->fb_height;
     fb.pitch = t->fb_pitch;
+
+    // no double buffering, the back and front buffers are the same
+    isdoublebuffered = false;
+    backbuffer = fb.addr;
+}
+
+void fb_enable_double_buffering()
+{
+    isdoublebuffered = true;
+    backbuffer = kmalloc(fb.pitch * fb.height);
+    memcpy_fast(fb.addr, backbuffer, fb.pitch * fb.height);
+}
+
+void fb_swap_buffers()
+{
+    if (isdoublebuffered)
+        memcpy_fast(backbuffer, fb.addr, fb.pitch * fb.height);
 }
