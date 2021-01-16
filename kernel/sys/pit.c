51d87198b4ce3f00cd86c22e6de1d7be8522e790
aditya <aarshisback@gmail.com>
2021-01-16 19:51:29 +0530

    added pit.c

---
+++ kernel/sys/pit.c
@@ -0,0 +1,23 @@
+#include "pit.h"
+#include "klog.h"
+#include "sys/cpu/cpu.h"
+#include <stdbool.h>
+
+#define TICK_LEN 0x04a9
+
+void pit_wait(uint64_t ms)
+{
+    while (ms--) {
+        port_outb(0x40, 0xa9);
+        port_outb(0x40, 0x04);
+
+        while (true) {
+            uint8_t lo, hi;
+            port_inb(0x40, &lo);
+            port_inb(0x40, &hi);
+            // check for overflow
+            if (hi > 0x04)
+                break;
+        }
+    }
+}
---
+++ kernel/sys/pit.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdint.h>
+
+void pit_wait(uint64_t ms);
