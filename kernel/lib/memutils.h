a6c09d026910ab35a6bb53e181810d1663eed345
aditya <aarshisback@gmail.com>
2021-02-11 12:47:58 +0530

    bugfix

--- kernel/lib/memutils.h
+++ kernel/lib/memutils.h
@@ -6,4 +6,3 @@
 bool memcmp(const void* s1, const void* s2, uint64_t len);
 void memset(void* addr, uint8_t val, uint64_t len);
 void memcpy(void* src, void* target, uint64_t len);
-void memcpy_fast(void* src, void* target, uint64_t len);
